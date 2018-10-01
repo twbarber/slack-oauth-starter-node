f849f3708f9ea8b6f61d92282ec3fcd399803d5b
Tyler Barber <dev@twbarber.com>
2018-10-01 08:36:20 -0400

    Initial Commit

---
+++ routes/oauth.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+router.get('/add', function (req, res, next) {
+  var authUrl = 'https://slack.com/oauth/authorize?' + 
+    'scope=' + 'incoming-webhook' + 
+    '&client_id=' + process.env.CLIENT_ID +
+    '&redirect_uri=' + process.env.REDIRECT_URI;
+  var button = {
+    alt: "Add to Slack",
+    height: "40",
+    width: "139",
+    src: "https://platform.slack-edge.com/img/add_to_slack.png",
+    srcset: "https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+  };
+  res.render('slack.pug', {
+    slackOauthUrl: authUrl,
+    button: button
+  });
+});
+
+router.get('/authorized', function (req, res, next) {
+
+  var options = {
+    uri: 'https://slack.com/api/oauth.access?code=' +
+      req.query.code +
+      '&client_id=' + process.env.CLIENT_ID +
+      '&client_secret=' + process.env.CLIENT_SECRET +
+      '&redirect_uri=' + process.env.REDIRECT_URI,
+    method: 'GET'
+  };
+
+  request(options, (error, response, body) => {
+    var JSONresponse = JSON.parse(body);
+    if (!JSONresponse.ok) {
+      console.log(JSONresponse);
+      res.send("Error encountered: \n" + JSON.stringify(JSONresponse)).status(200).end()
+    } else {
+      console.log(JSONresponse);
+      res.send("Success!");
+    }
+  });
+
+});
+
+module.exports = router;
